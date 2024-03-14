d303191820ff55bdf73214bca9f90e2060b1779f
RRReu <161722638+RRReu@users.noreply.github.com>
2024-03-14 16:28:13 +0300

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,16 @@
+
+i = float(input("Bir sayı giriniz."))
+if ( i < 0 ):
+    print("Bu sayı negatiftir.")
+elif ( i > 0 ):
+    print("Bu sayı pozitiftir.")
+else:
+    print("Bu sayı sıfırdır.")
+
+
+x = int(input("Bir sayı giriniz"))
+if ( x < 0):
+    if(x % 2 ==0):
+        print("Girdiğiniz sayı pozitif çift sayıdır")
+            else:
+                print("Girdiğiniz sayı pozitif tek sayıdır")
---
+++ if-else1.py
@@ -0,0 +1,15 @@
+i = int(input("Bir sayı giriniz."))
+
+if ( x > 0):
+    if (x % ==0):
+        print("Girdiğiniz sayı pozitif çift sayıdır.")
+            else:
+                print("Girdiğiniz sayı pozitif tek sayıdır.")
+
+elif (x <0):
+    if ( x % ==0)
+        print("Girdiğiniz sayı negatif çift sayıdır.")
+            else:
+                print("Girdiğiniz sayı negatif tek sayıdır.")
+else:
+    print("Girdiğiniz sayı sıfırdır.")
