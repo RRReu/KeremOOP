d566e1626cd7b7834c31f4464b41b9a5f1359058
RRReu <161722638+RRReu@users.noreply.github.com>
2024-04-25 16:28:08 +0300

    Add files via upload

---
+++ modul deneme.py
@@ -0,0 +1,12 @@
+from module import x as p
+"""print(p.x)
+print(p.y)
+print(p.y[1])
+a = p.y[0]
+print(a*100)
+b = p.y[2]
+print(b*100)
+!!! X yerine direkt module import edilirse burası çalışır.
+"""
+"""!!! from module import * şeklinde yaparsan bütün bileşenler içeri alınır."""
+print(p)
